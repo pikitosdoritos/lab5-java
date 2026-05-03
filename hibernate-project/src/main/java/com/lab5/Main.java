5b91b9751e407ef21c8e01c4317a7226ae86d436
pikitosdoritos <fastovetsn@gmail.com>
2026-05-04 00:36:40 +0300

    Implement Hibernate session management in Main class

--- hibernate-project/src/main/java/com/lab5/Main.java
+++ hibernate-project/src/main/java/com/lab5/Main.java
@@ -1,7 +1,24 @@
 package com.lab5;
 
+import com.lab5.util.HibernateUtil;
+import org.hibernate.Session;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Starting Hibernate...");
+
+        Session session = HibernateUtil
+                .getSessionFactory()
+                .openSession();
+
+        session.beginTransaction();
+
+        session.getTransaction().commit();
+
+        session.close();
+
+        System.out.println("Done.");
     }
 }
