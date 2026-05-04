324028546b02bc4e6921e6aecb34a438467f9df3
pikitosdoritos <fastovetsn@gmail.com>
2026-05-04 22:47:36 +0300

    Refactor StudentDAO: rename saveStudent to save, implement transaction management, and add delete method

--- hibernate-project/src/main/java/com/lab5/Main.java
+++ hibernate-project/src/main/java/com/lab5/Main.java
@@ -2,6 +2,7 @@ package com.lab5;
 
 import com.lab5.dao.StudentDAO;
 import com.lab5.model.Student;
+import com.lab5.model.StudentProfile;
 
 public class Main {
 
@@ -12,8 +13,11 @@ public class Main {
         StudentDAO studentDAO = new StudentDAO();
 
         Student student = new Student("Nikita", 19);
+        StudentProfile profile = new StudentProfile("nikita@mail.com", "123456");
 
-        studentDAO.saveStudent(student);
+        student.setProfile(profile);
+
+        studentDAO.save(student);
 
         System.out.println("Done.");
     }
--- hibernate-project/src/main/java/com/lab5/dao/StudentDAO.java
+++ hibernate-project/src/main/java/com/lab5/dao/StudentDAO.java
@@ -3,24 +3,103 @@ package com.lab5.dao;
 import com.lab5.model.Student;
 import com.lab5.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
 
 public class StudentDAO {
 
-    // INSERT
-    public void saveStudent(Student student) {
+    // CREATE
+    public void save(Student student) {
 
-        Session session = HibernateUtil
-                .getSessionFactory()
-                .openSession();
+        Transaction transaction = null;
 
-        session.beginTransaction();
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
 
-        session.persist(student);
+            transaction = session.beginTransaction();
 
-        session.getTransaction().commit();
+            session.persist(student);
 
-        session.close();
+            transaction.commit();
 
-        System.out.println("Student saved!");
+            System.out.println("Student saved!");
+
+        } catch (Exception e) {
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+        }
+    }
+
+    // READ BY ID
+    public Student getById(Long id) {
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.get(Student.class, id);
+        }
+    }
+
+    // READ ALL
+    public List<Student> getAll() {
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Student", Student.class).list();
+        }
+    }
+
+    // UPDATE
+    public void update(Student student) {
+
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            session.merge(student);
+
+            transaction.commit();
+
+            System.out.println("Student updated!");
+
+        } catch (Exception e) {
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+        }
+    }
+
+    // DELETE
+    public void delete(Long id) {
+
+        Transaction transaction = null;
+
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+
+            transaction = session.beginTransaction();
+
+            Student student = session.get(Student.class, id);
+
+            if (student != null) {
+                session.remove(student);
+                System.out.println("Student deleted!");
+            }
+
+            transaction.commit();
+
+        } catch (Exception e) {
+
+            if (transaction != null) {
+                transaction.rollback();
+            }
+
+            e.printStackTrace();
+        }
     }
 }
