148d3ced2d1ac099749358e202a940147ec55e86
pikitosdoritos <fastovetsn@gmail.com>
2026-05-04 22:48:19 +0300

    Enhance Main class: link student with test and save student to database

--- hibernate-project/src/main/java/com/lab5/Main.java
+++ hibernate-project/src/main/java/com/lab5/Main.java
@@ -1,8 +1,7 @@
 package com.lab5;
 
 import com.lab5.dao.StudentDAO;
-import com.lab5.model.Student;
-import com.lab5.model.StudentProfile;
+import com.lab5.model.*;
 
 public class Main {
 
@@ -12,10 +11,36 @@ public class Main {
 
         StudentDAO studentDAO = new StudentDAO();
 
+        // ---------- CREATE STUDENT + PROFILE ----------
+
         Student student = new Student("Nikita", 19);
         StudentProfile profile = new StudentProfile("nikita@mail.com", "123456");
 
         student.setProfile(profile);
+
+        // ---------- CREATE TUTOR ----------
+
+        Tutor tutor = new Tutor("Ivan Petrov", "OOP");
+
+        // ---------- CREATE TEST ----------
+
+        TestEntity test = new TestEntity("OOP Test", "Java");
+
+        tutor.addTest(test);
+
+        // ---------- CREATE QUESTIONS ----------
+
+        Question q1 = new Question("What is OOP?", "Concept");
+        Question q2 = new Question("What is encapsulation?", "Data hiding");
+
+        test.addQuestion(q1);
+        test.addQuestion(q2);
+
+        // ---------- LINK STUDENT WITH TEST ----------
+
+        student.addTest(test);
+
+        // ---------- SAVE ----------
 
         studentDAO.save(student);
 
